9d244233eda16f1ff675be75566ca13852c2b416
MacMini <macmini@MacMinis-Mac-mini.local>
2022-07-03 20:28:27 +0530

    Counter created with basic functionalities

--- src/component/Counter/index.js
+++ src/component/Counter/index.js
@@ -5,21 +5,42 @@ import React,{useState,useEffect} from 'react';
 //acccpet the props 
 //duration(in sec) , callBack
 
-const Counter=(props)=>{
+ const Counter=(props)=>{
    
     const {duration, callBack}=props;
 
-    const [timer,startTimer]=useState(0);
+    const [timer,startTimer]=useState(duration);
 
 
+  console.log(timer );
+
+  
+
+   useEffect(()=>{
+   
+    if(!timer)
+    {
+       console.log("drfdrf")
+        if(callBack)
+        callBack();
+       
+     return;
+    }
+
+   const id= setTimeout(()=>{
+    startTimer(timer-1000)
+   },1000);
+
+   return ()=>{
+
+      clearTimeout(id);
+
+   };
+
+   },[timer])
 
 
-
-return(
-    <main>
-        <h2>{counter}</h2>
-    </main>
-)
+return getFormatedTime(timer);
 
 };
 
@@ -27,16 +48,17 @@ return(
 export default Counter;
 
 const getFormatedTime=(duration)=>{
-
+  console.log("Duration",duration)
     //1000 miliSec means 1sec
 
-    // min, hour , sec, days
-    const min=Math.floor(duration/60000);
-    const hour=Math.floor(duration/600000);
-   const days=Math.floor(duration/(600000*24));
-    const sec=Math.floor(duration/1000);
+    // min, hour , sec, days 
+    const sec=Math.floor((duration/1000)%60);
+    const min=Math.floor((duration/60000)%60);
+    const hour=Math.floor((duration/3600000)%24);
+   const days=Math.floor(duration/(3600000*24));
+    
 
-return `${days} : ${hour} : ${min} : ${sec}`
+return `${days}  ${hour}  ${min}  ${sec}`;
 
 }
 
